af5503a4a6dbef7a415164653c90a14b624c18ee
Telvo <telvowarrior@gmail.com>
2023-12-11 16:57:41 +0300

    some changes

--- 02_overview-of-the-go-language/packages/go.mod
+++ 02_overview-of-the-go-language/packages/go.mod
@@ -1,3 +1,3 @@
-module packages
+module github.com/TelvoWarrior/Building-Modern-Web-Applications-with-Go-Golang/tree/main/02_overview-of-the-go-language/packages
 
 go 1.21.5
--- 02_overview-of-the-go-language/packages/main.go
+++ 02_overview-of-the-go-language/packages/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "log"
+import (
+	"github.com/TelvoWarrior/Building-Modern-Web-Applications-with-Go-Golang/tree/main/02_overview-of-the-go-language/packages/helpers"
+	"log"
+)
 
 func main() {
 	log.Println("Hello, Packages!")
+
+	var myVar helpers.SomeType
+	myVar.TypeName = "This is type name"
+	log.Println(myVar)
+
 }
