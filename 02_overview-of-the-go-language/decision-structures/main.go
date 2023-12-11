51b255b0d6838df7ae89d416370e75ee91003250
Telvo <telvowarrior@gmail.com>
2023-12-11 14:09:10 +0300

    complete decision-structures section

--- 02_overview-of-the-go-language/decision-structures/main.go
+++ 02_overview-of-the-go-language/decision-structures/main.go
@@ -4,4 +4,26 @@ import "log"
 
 func main() {
 	log.Println("Hello, Decision Structures!")
+
+	var isTrue bool
+	isTrue = false
+
+	if isTrue {
+		log.Println("isTrue is:", isTrue)
+	} else {
+		log.Println("isTrue is:", isTrue)
+	}
+
+	myVar := "zergling"
+
+	switch myVar {
+	case "cat":
+		log.Println("It's a cat.")
+	case "dog":
+		log.Println("It's a dog.")
+	case "fish":
+		log.Println("It's a fish.")
+	default:
+		log.Println("It's an unknown creature.")
+	}
 }
