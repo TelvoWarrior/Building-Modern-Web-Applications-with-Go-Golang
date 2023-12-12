a6225b2e5bc1f45f1ff3a1f7286451ead4342c43
Telvo <telvowarrior@gmail.com>
2023-12-12 10:10:13 +0300

    complete making-a-simple-web-app section

--- 03_building-a-basic-web-application/making-a-simple-web-application/main.go
+++ 03_building-a-basic-web-application/making-a-simple-web-application/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+)
+
+const PORT = ":8080"
 
 func main() {
-	log.Println("Hello, Web App!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		n, err := fmt.Fprintf(w, "Hello, Web App!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(PORT, nil)
 }
